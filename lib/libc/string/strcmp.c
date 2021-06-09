730f55c08630c3eff7c1e9b0b9867712528c9a6c
gAlfonso-bit <83477269+gAlfonso-bit@users.noreply.github.com>
2021-06-08 21:16:35 -0400

    Update strcmp.c

--- lib/libc/string/strcmp.c
+++ lib/libc/string/strcmp.c
@@ -40,9 +40,11 @@
 int
 strcmp(const char *s1, const char *s2)
 {
-	while (*s1 == *s2++)
-		if (*s1++ == 0)
-			return (0);
-	return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+	while (*s1 != '\0') {
+		if (*s1 != *s2)
+			return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+		s1++, s2++;
+	}
+	return (0);
 }
 DEF_STRONG(strcmp);
